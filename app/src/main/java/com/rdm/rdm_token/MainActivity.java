29a74162f852af2559aa7b096beb2035dce73f5b
Dell <prathmeshranpisea07@gmail.com>
2022-12-05 07:15:23 +0530

    Final commit

---
+++ app/src/main/java/com/rdm/rdm_token/MainActivity.java
@@ -0,0 +1,41 @@
+package com.rdm.rdm_token;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ico.rdmexchange.com/");
+
+       WebSettings webSettings = webView.getSettings();
+       webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    private static class webSettings {
+    }
+}
